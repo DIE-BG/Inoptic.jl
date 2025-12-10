8c4c426860b6af17a4198b774734f07e63048c29
Rubí Ramírez <rubiesramirez@gmail.com>
2025-12-10 14:21:25 -0600

    feat: Add deps to repository.

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,25 @@
 using Inoptic
 using Documenter
 
-DocMeta.setdocmeta!(Inoptic, :DocTestSetup, :(using Inoptic); recursive=true)
+DocMeta.setdocmeta!(Inoptic, :DocTestSetup, :(using Inoptic); recursive = true)
 
 makedocs(;
-    modules=[Inoptic],
-    authors="DIE-BG contributors",
-    sitename="Inoptic.jl",
-    format=Documenter.HTML(;
-        edit_link="main",
-        assets=String[],
+    modules = [Inoptic],
+    authors = "DIE-BG contributors",
+    repo = "https://github.com/DIE-BG/Inoptic.jl/blob/{commit}{path}#{line}",
+    sitename = "Inoptic.jl",
+    format = Documenter.HTML(;
+        edit_link = "main",
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://die-bg.github.io/Inoptic.jl",
+        assets = ["assets/logo.ico"],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
     ],
+)
+
+deploydocs(;
+    repo = "github.com/DIE-BG/Inoptic.jl",
+    devbranch = "main",
 )
