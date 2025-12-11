f8de30addd841e6c61edf35ec5dbc19ed1342ad6
Rubí Ramírez <rubiesramirez@gmail.com>
2025-12-11 15:57:12 -0600

    chore: Do not load CPIDataGT data on module

--- src/Inoptic.jl
+++ src/Inoptic.jl
@@ -7,11 +7,9 @@ using CPIDataGT
 
 using DataFrames
 set_language!(:english)
-CPIDataGT.load_data()
 
 include("helpers.jl")
-# Load GT data
-#CPIDataGT.load_data()
+
 
 ## Optimal functions for 2021 edition -------------------------------
 # Load GT data for last calibration 2021 version
